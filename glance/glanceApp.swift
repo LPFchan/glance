668a4874de4d0ccf2c0b801eb1ed3dacd44282a2
Jonathan Zhou <jonnyzgg@gmail.com>
2026-07-21 15:14:37 -0400

    Initial Commit

---
+++ glance/glanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  glanceApp.swift
+//  glance
+//
+//  Created by Jonathan Zhou on 2026-07-21.
+//
+
+import SwiftUI
+
+@main
+struct glanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
